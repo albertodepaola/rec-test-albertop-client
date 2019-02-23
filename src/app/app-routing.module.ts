dfb33ccf8fc3558d27fdc732415e754f0d049ab5
Alberto de Paola <alberto.paola@cwi.com.br>
2019-02-23 00:38:39 -0300

    partial profile component, needs design now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SearchComponent } from './search/search.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   {path: 'home', component: SearchComponent},
   {path: '', component: SearchComponent},
   {path: 'torreApp', component: SearchComponent},
-  {path: 'search', component: SearchComponent}
+  {path: 'search', component: SearchComponent},
+  {path: 'profile/:username', component: ProfileComponent}
 ];
 
 @NgModule({
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import { TorreUserService } from '../torre-user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  username: string;
+  user: Object;
+
+  constructor(private route: ActivatedRoute, private torreUserService: TorreUserService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.username = params['username'];
+      this.torreUserService.getUserByUsername(this.username).subscribe(data => {
+        this.user = data;
+        console.log(this.user);
+      });
+    });
   }
 
 }
